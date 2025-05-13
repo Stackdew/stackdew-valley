5ff89ebcb034e623b9602c4fce8b2b1cfbc7b3e1
Ken'Terria Reaves <kenterria.z.reaves@gmail.com>
2025-05-13 16:01:58 +0100

    feat: add enemyDevlingData array with stats, sprites and sounds

---
+++ src/components/game/enemyDevlingData.js
@@ -0,0 +1,66 @@
+export const enemyDevlings = [
+  {
+    name: "Norbion",
+    stats: {
+      frontend: 1,
+      backend: 4,
+      "dev ego": 2,
+      commits: 2,
+      debugging: 3,
+      Resilience: 5,
+    },
+
+    sprite: "../assets/battleHeads/Noah.png",
+    nameSound: "../assets/sounds/norbion.mp3",
+  },
+  {
+    name: "flyor",
+    stats: {
+      frontend: 1,
+      backend: 5,
+      "dev ego": 3,
+      commits: 3,
+      debugging: 4,
+      Resilience: 2,
+    },
+
+    sprite: "../assets/battleHeads/Flyor.png",
+
+    nameSound: "../assets/sounds/norbion.mp3",
+  },
+  {
+    name: "Becca",
+    stats: {
+      frontend: 5,
+      backend: 1,
+      "dev ego": 4,
+      commits: 3,
+      debugging: 0,
+      Resilience: 3,
+    },
+
+    sprite: "../assets/battleHeads/soph.png",
+
+    nameSound: "../assets/sounds/beccaSound.mp3",
+    isPlanted: false,
+    isWatered: false,
+    isGrown: false,
+  },
+  {
+    name: "Zeke",
+    stats: {
+      frontend: 5,
+      backend: 1,
+      "dev ego": 4,
+      commits: 3,
+      debugging: 0,
+      Resilience: 3,
+    },
+    sprite: "../assets/battleHeads/Zeke.png",
+
+    nameSound: "../assets/sounds/beccaSound.mp3",
+    isPlanted: false,
+    isWatered: false,
+    isGrown: false,
+  },
+];
